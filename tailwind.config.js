ef3bc480b301737147ceea511afa46d8080e403e
rushikeshtaur12 <rushikeshtaur12@gmail.com>
2025-09-04 18:40:59 +0530

    changes for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.js",],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // include all JS/JSX/TS/TSX files in src
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
